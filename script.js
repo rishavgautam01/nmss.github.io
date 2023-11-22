5efce0a0c7e5afc62544d53fd48422980e666591
rishavgautam01 <130028453+rishavgautam01@users.noreply.github.com>
2023-11-22 23:03:05 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.forms['form'];
+    const usernameInput = document.getElementById('username_input');
+    const passwordInput = document.getElementById('password_input');
+    const usernameError = document.getElementById('username_error');
+    const passwordError = document.getElementById('password_error');
+
+    form.addEventListener('submit', function (event) {
+        // Prevent the form from submitting
+        event.preventDefault();
+
+        // Reset previous error messages
+        usernameError.textContent = '';
+        passwordError.textContent = '';
+
+        // Get the values entered by the user
+        const username = usernameInput.value.trim();
+        const password = passwordInput.value.trim();
+
+        // Simple validation
+        if (username === '') {
+            usernameError.textContent = 'Username is required';
+            return;
+        }
+
+        if (password === '') {
+            passwordError.textContent = 'Password is required';
+            return;
+        }
+
+        // For simplicity, you can check a hardcoded username and password
+        const validUsername = 'exampleuser' ;
+        const validPassword = 'examplepassword';
+
+        // Check if the entered credentials are valid
+        if (username === validUsername && password === validPassword) {
+            // Redirect based on screen width
+            if (window.innerWidth <= 600) {
+                window.location.href = '/home.html';
+            } else {
+                window.location.href = '/main.html';
+            }
+        } else {
+            // Display an error message for incorrect credentials
+            passwordError.textContent = 'Incorrect username or password';
+        }
+        if(username==='admin' && password==='pass'){
+            window.location.href='admin.html';
+        }
+    });
+});
+y
